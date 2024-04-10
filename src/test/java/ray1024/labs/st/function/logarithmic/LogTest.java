c0d10c314fb61348f3854db333e97c625616a8d7
Ray_1024 <skovorodnikovda@mail.ru>
2024-04-10 22:24:58 +0300

    add empty test classes

---
+++ src/test/java/ray1024/labs/st/function/logarithmic/LogTest.java
@@ -0,0 +1,11 @@
+package ray1024.labs.st.function.logarithmic;
+
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class LogTest {
+
+}
---
+++ src/test/java/ray1024/labs/st/function/trigonimetric/SinTest.java
@@ -0,0 +1,11 @@
+package ray1024.labs.st.function.trigonimetric;
+
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SinTest {
+
+}
